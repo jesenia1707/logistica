693771d7f1f1efdc0c9a1c24ba9e74d9ae8f797b
Barbara-Carolina7 <ba.arayac@duocuc.cl>
2026-06-18 00:57:51 -0400

    agregando codigo

--- logistica/src/main/java/com/donaton/logistica/LogisticaApplication.java
+++ logistica/src/main/java/com/donaton/logistica/LogisticaApplication.java
@@ -2,12 +2,16 @@ package com.donaton.logistica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.donaton.logistica.repository")
+@EntityScan(basePackages = "com.donaton.logistica.entity")
 public class LogisticaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LogisticaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticaApplication.class, args);
+    }
 }
+
